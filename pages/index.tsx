afa005805821b3606e87534a2466eb2501fdbc3d
camilon0 <kmilo8433@gmail.com>
2024-12-18 18:43:24 -0500

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import React from "react";
 import App from "../app/App";
 import "./styles/styles.css";
 const HomePage = () => {
   return (
     <div>
+      <Head>
+        <title>EasyReserves</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <App />
     </div>
   );
